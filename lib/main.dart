53e6b5616b0de626763ccabe617a82c4370c4b91
Agata Fontes <fontesgomesagata@gmail.com>
2026-07-24 21:43:45 -0300

    Cria  pasta de assets -adiciona a fonte Inter - aplica um espaçamento entre a logo

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ class Login extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Row(children: [FlutterLogo(size: 18), Text("ChatSENAC")]),
+        SizedBox(height: 32,),
+
         // Titulos
         Text("Entre na sua conta",
         style: Tipografia.h1
